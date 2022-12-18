f713a9ec271c5791fec4947c22d462ecc899a2fe
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-12-18 16:25:09 +0300

    Feat: add empty favourites page

--- src/pages/favourites/Favourites.js
+++ src/pages/favourites/Favourites.js
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
 import './Favourites.scss';
+import sadFavourite from '../../images/sad-fafourite.png';
+import arrowBack from '../../images/arrow-back.png';
 
 export const Favourites = () => {
-	return <div>Favourites</div>;
+	return (
+		<div className='favourite'>
+			<img className='favourite__image' src={sadFavourite} alt='' />
+			<h1 className='favourite__title'>Закладок нет :(</h1>
+			<p className='favourite__description'>Вы ничего не добавляли в закладки</p>
+			<Link to='/'>
+				<button className='favourite__button' type='button'>
+					<img className='favourite__button-image' src={arrowBack} alt='Назад' />
+					<span>Вернуться назад</span>
+				</button>
+			</Link>
+		</div>
+	);
 };
