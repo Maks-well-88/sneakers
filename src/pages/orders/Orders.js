bf21fd211b9ea1c7c22f8f51718f6d94a0ef5975
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-12-18 16:55:49 +0300

    Feat: add empty orders page

--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
 import './Orders.scss';
+import sadOrders from '../../images/sad-orders.png';
+import arrowBack from '../../images/arrow-back.png';
 
 export const Orders = () => {
-	return <div>Orders</div>;
+	return (
+		<div className='orders'>
+			<img className='orders__image' src={sadOrders} alt='Нет заказов' />
+			<h1 className='orders__title'>У вас нет заказов</h1>
+			<p className='orders__description'>Вы нищеброд?</p>
+			<p className='orders__description'>Оформите хотя бы один заказ.</p>
+			<Link to='/'>
+				<button className='orders__button' type='button'>
+					<img className='orders__button-back' src={arrowBack} alt='Назад' />
+					<span>Вернуться назад</span>
+				</button>
+			</Link>
+		</div>
+	);
 };
